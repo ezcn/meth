db678f7df90e6b9a4d980d31f8a21ddfadfa79ea
Enza Colonna <enza.colonna@gmail.com>
2022-09-14 17:43:55 +0200

    Create annotatePycomethReport.py

---
+++ annotatePycomethReport.py
@@ -0,0 +1,25 @@
+import sys, ast, re, numpy as np
+
+
+def closest (values, N): 
+        X = np.array(values).astype(np.float32)
+        d = np.abs(X[None, :] - X[:, None])
+        np.fill_diagonal(d, np.inf)
+        min_array = d.min(0)
+        par = np.argpartition(min_array, N)
+        closestvalues=X[par[:N]]
+        return closestvalues
+
+myfile = sys.argv[sys.argv.index("--file") + 1]        
+with open(myfile) as f:
+    next(f)
+    for line in f:
+        if re.search('Significant pvalue', line): 
+            x=line.rstrip().split('\t') 
+            labels=ast.literal_eval(x[10]); values=ast.literal_eval(x[11])
+            tempd=dict(zip(map(str,values), labels))
+            myclose=closest (values, 2)
+            mypair=['-'.join(sorted([tempd[str(i)] for i in myclose]))] 
+            #print(myclose)
+            state=['Unmethylated' if (all(c < 0 for c in myclose)) else 'Methylated']
+            print ('\t'.join(mypair + state + x) )
